6b56081e1d4cf80f41d1ac29b7729a67e76e83ed
Aleksandr Babushkin <i@e0m.ru>
2024-02-01 12:45:24 +0300

    Improves stylesheats

--- static/js/menu.js
+++ static/js/menu.js
@@ -1,14 +1,6 @@
-window.addEventListener('scroll', function () {
-    var shrinkHeader = 1;
-    // var header = document.querySelector('.header');
-    var logo = document.querySelector('#logo');
-    var scroll = window.scrollY || document.documentElement.scrollTop;
+var link = document.querySelector('#menu_trigger');
+var elem = document.querySelector('#menu');
 
-    if (scroll >= shrinkHeader) {
-        // header.classList.add('shrink');
-        logo.classList.toggle('big');
-    } else {
-        // header.classList.remove('shrink');
-        logo.classList.toggle('big');
-    }
+link.addEventListener('click', function () {
+    elem.classList.toggle('show')
 });
